005c5363b46068f044a57cd7de5609a5dfce5778
Gurdeep Kullar <gkullar.27@gmail.com>
2020-04-20 10:44:07 +0100

    add global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,14 @@ const Home: NextPage<{ userAgent: string }> = () => (
       <title>Exercise</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
     </Head>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
     <p>Hello world!</p>
   </div>
 );
