bcf1a139488d5ffdd10a28689fda9c52e83bd565
Gurdeep Kullar <gkullar.27@gmail.com>
2020-04-20 09:23:37 +0100

    add head tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+const Home: NextPage<{ userAgent: string }> = () => (
+  <div>
+    <Head>
+      <title>Exercise</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
 );
-
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
-};
 
 export default Home;
